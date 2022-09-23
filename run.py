108e238e9ec1b3a42ff02b04ca443abe6403744a
Mattias-b-Pettersson <mattep3@gmail.com>
2022-09-23 09:01:47 +0000

    Added the draw function and init_game function

--- run.py
+++ run.py
@@ -1,3 +1,38 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def draw():
+    """
+    Draws the game board
+    """
+    board = ""
+    for k, v in board_state.items():
+
+        if k == "A1":
+            print("    A   B   C")
+            board += "1 "
+            board += f"{v} "
+        elif k == "C1":
+            board += f"{v} \n"
+            board += "  ------------- \n2 "
+        elif k == "C2":
+            board += f"{v} \n"
+            board += "  ------------- \n3 "
+        else:
+            board += f"{v} "
+    print(board)
+
+def init_game():
+    print("Welcome to tic tac toe!")
+
+    global board_state
+    board_state = {
+        "A1": "| -",
+        "B1": "| - |",
+        "C1": "- |",
+        "A2": "| -",
+        "B2": "| - |",
+        "C2": "- |",
+        "A3": "| -",
+        "B3": "| - |",
+        "C3": "- |"}
+    draw()
+
+init_game()
